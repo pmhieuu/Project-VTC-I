a8b264a90fa520e71c6dd01d59c98c40a283eeee
pmhieuu <pmhieu1409@gmail.com>
2023-04-06 20:11:07 +0700

    Change account username

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,8 +1,7 @@
 ﻿namespace Models;
 public class Account
 {
-  public string Customer_Username { get; set; }
-  public string Staff_Username { get; set; }
+  public string Username { get; set; }
   public string Password { get; set; }
   public int Role { get; set; }
 }
