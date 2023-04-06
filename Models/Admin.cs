f6a08ea48e71fd25495868562573423860cb0e53
pmhieuu <pmhieu1409@gmail.com>
2023-04-06 19:49:13 +0700

    Add Models Account, Admin, Category, Product

---
+++ Models/Admin.cs
@@ -0,0 +1,11 @@
+namespace Models;
+
+public class Admin
+{
+  public int ID { get; set; }
+  public string Username { get; set; }
+  public string Fullname { get; set; }
+  public string Email { get; set; }
+  public string Status { get; set; }
+  public string Phone_Number { get; set; }
+}
